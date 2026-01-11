0c66cb58347da99bf7e6f5c19861529e734549f8
Matt Tingley <mtingley99@gmail.com>
2026-01-11 19:21:26 -0400

    Component Adds with additional page.

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,7 +1,26 @@
+
 const navLinks = document.querySelector('.nav-links');
 const hamburger = document.querySelector('.hamburger');
+const menuLinks = document.querySelectorAll('.menuLink');
+const service = document.querySelector('.services');
+const servicesList = document.querySelector('.services-list');
 
-	hamburger.addEventListener('click', () => {
-		hamburger.classList.toggle('is-active');
-		navLinks.classList.toggle('w-full');
-	});
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('is-active');
+	navLinks.classList.toggle('w-full');
+});
+
+for (var i = 0; i < menuLinks.length; i++) {
+	menuLinks[i].addEventListener('click', () => {
+		if (hamburger.classList.contains('is-active') && navLinks.classList.contains('w-full')) {
+			hamburger.classList.remove('is-active');
+			navLinks.classList.remove('w-full');
+		}
+	}
+	)
+};
+
+service.addEventListener('click', () => {
+	servicesList.classList.toggle('openMenu');
+});
+
